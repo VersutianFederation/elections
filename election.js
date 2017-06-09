9fc96a285494c1a13f6aa9420cc83c80159961cd
mastercoms <circuitsoft@tuta.io>
2017-06-09 12:07:31 -0400

    add new case for sign out

--- election.js
+++ election.js
@@ -220,7 +220,7 @@ function clearElections() {
     "use strict";
     var elections = document.getElementById('elections');
     elections.innerHTML = '<h1>Vote</h1>';
-    elections.innerHTML += '<p class="lead">Welcome, <img style="max-height: 13px; max-width: 20px; margin-right: 4px" src="' + yourNationFlag + '">' + nationName + '. Vote on these elections:</p><p><small>Not ' + nationName + '? </small> <button class="btn btn-secondary btn-sm" onclick="signOut()">sign out</button></p>';
+    elections.innerHTML += '<p class="lead">Welcome, <img style="max-height: 13px; max-width: 20px; margin-right: 4px" src="' + yourNationFlag + '">' + nationName + '. Vote on these elections:</p><p><small>Not ' + nationName + ' or on a public device? </small> <button class="btn btn-secondary btn-sm" onclick="signOut()">sign out</button></p>';
 }
 
 function loginembed() {
